564d861d13fee081a07d5741cbfd4532ce37d1bb
teatracer <teatracer7@yandex.ru>
2017-10-13 17:32:46 +0300

    Add ApproximateNearest function

--- task2/main.go
+++ task2/main.go
@@ -8,9 +8,53 @@ import (
 	"os"
 )
 
+func ApproximateNearest(img *image.Gray) (out *image.Gray) {
+	// get image bounds
+	rectangle := (*img).Bounds()
+
+	// create output image
+	out = image.NewGray(rectangle)
+
+	for y := rectangle.Min.Y; y < rectangle.Max.Y; y++ {
+		for x := rectangle.Min.X; x < rectangle.Max.X; x++ {
+
+			grey_value := (*img).GrayAt(x, y)
+			if grey_value.Y < 177 {
+				grey_value.Y = 0
+			} else {
+				grey_value.Y = 255
+			}
+
+			out.SetGray(x, y, grey_value)
+		}
+	}
+	return
+}
+
+func MakeImageGray(img *image.Image) (out *image.Gray) {
+
+	// get image bounds
+	rectangle := (*img).Bounds()
+
+	// create output image
+	out = image.NewGray(rectangle)
+
+	// iterate over all points
+	for y := rectangle.Min.Y; y < rectangle.Max.Y; y++ {
+		for x := rectangle.Min.X; x < rectangle.Max.X; x++ {
+
+			full_color := (*img).At(x, y)
+			c := color.GrayModel.Convert(full_color).(color.Gray)
+			out.Set(x, y, c)
+		}
+	}
+	return
+}
+
 func main() {
 	input_img_ptr := flag.String("i", "input.png", "Input PNG image.")
 	output_img_ptr := flag.String("o", "output.png", "Output PNG image.")
+	// approximation_type := flag.String("a", "default", "Approximation type (default='nearest')")
 	flag.Parse()
 
 	// load file
@@ -19,23 +63,10 @@ func main() {
 	// decode png image
 	input_img, _ := png.Decode(input_img_raw)
 
-	// get image bounds
-	rectangle := input_img.Bounds()
-
 	// create output file
 	output_img_raw, _ := os.Create(*output_img_ptr)
 
-	// create output image
-	output_img := image.NewGray(rectangle)
-
-	// iterate over all points
-	for y := rectangle.Min.Y; y < rectangle.Max.Y; y++ {
-		for x := rectangle.Min.X; x < rectangle.Max.X; x++ {
-
-			full_color := input_img.At(x, y)
-			c := color.GrayModel.Convert(full_color).(color.Gray)
-			output_img.Set(x, y, c)
-		}
-	}
-	png.Encode(output_img_raw, output_img)
+	output_img := MakeImageGray(&input_img)
+	output_img = ApproximateNearest(output_img)
+	png.Encode(output_img_raw, image.Image(output_img))
 }
